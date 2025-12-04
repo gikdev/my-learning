0b021b6feda0b22c8b3021eda902005e9b085990
gikdev <m.m.bhrm85@gmail.com>
2025-12-04 10:47:35 +0330

    Start AI Assisted coding experiment

---
+++ experiments/ai-assisted-coding/GymManagement/src/GymManagement.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,5 @@
+﻿namespace GymManagement.Application.Common.Interfaces;
+
+public interface IUnitOfWork {
+    Task CommitChangesAsync();
+}
---
+++ experiments/ai-assisted-coding/GymManagement/src/GymManagement.Domain/Subscriptions/Subscription.cs
@@ -0,0 +1,22 @@
+﻿namespace GymManagement.Domain.Subscriptions;
+
+public class Subscription {
+    private readonly Guid _adminId;
+    public Guid Id { get; private set; }
+    public SubscriptionType SubscriptionType { get; private set; }
+
+    public Subscription(
+        SubscriptionType subscriptionType,
+        Guid adminId,
+        Guid? id = null
+    ) {
+        SubscriptionType = subscriptionType;
+        _adminId = adminId;
+        Id = id ?? Guid.NewGuid();
+    }
+
+    // ReSharper disable once UnusedMember.Local
+    #pragma warning disable CS8618
+    private Subscription() {}
+    #pragma warning restore CS8618
+}
