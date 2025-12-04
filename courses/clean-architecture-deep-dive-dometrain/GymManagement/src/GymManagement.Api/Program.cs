6b9cdf7598c2ce79a3a6d81dbe5dbcb9988228d1
gikdev <m.m.bhrm85@gmail.com>
2025-12-04 17:39:13 +0330

    Set project up for the course.

---
+++ courses/clean-architecture-deep-dive-dometrain/GymManagement/src/GymManagement.Api/Program.cs
@@ -0,0 +1,21 @@
+using GymManagement.Application;
+using GymManagement.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddProblemDetails();
+
+builder.Services
+       .AddApplication()
+       .AddInfrastructure();
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
