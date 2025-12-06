30f5611f85a3fbb46a6dac2bd4d67229da02425a
gikdev <m.m.bhrm85@gmail.com>
2025-12-06 19:01:07 +0330

    Learn how to add FluentValidation to App layer.

--- courses/clean-architecture-getting-started-dometrain/Todos/src/App/DependencyInjection.cs
+++ courses/clean-architecture-getting-started-dometrain/Todos/src/App/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using App.Common.Behaviors;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace App;
@@ -6,7 +8,11 @@ public static class DependencyInjection {
     public static IServiceCollection AddApp(this IServiceCollection services) {
         services.AddMediatR(options => {
             options.RegisterServicesFromAssemblyContaining(typeof(DependencyInjection));
+
+            options.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
 
         return services;
     }
--- courses/clean-architecture-getting-started-dometrain/Todos/src/App/Todos/Commands/ChangeTodoImportance/ChangeTodoImportanceCommandHandler.cs
+++ courses/clean-architecture-getting-started-dometrain/Todos/src/App/Todos/Commands/ChangeTodoImportance/ChangeTodoImportanceCommandHandler.cs
@@ -8,8 +8,10 @@ public class ChangeTodoImportanceCommandHandler(
     ITodosRepository todosRepository,
     IUnitOfWork unitOfWork
 ) : IRequestHandler<ChangeTodoImportanceCommand, ErrorOr<Success>> {
-    public async Task<ErrorOr<Success>>
-        Handle(ChangeTodoImportanceCommand request, CancellationToken cancellationToken) {
+    public async Task<ErrorOr<Success>> Handle(
+        ChangeTodoImportanceCommand request,
+        CancellationToken cancellationToken
+    ) {
         var todo = await todosRepository.GetByIdAsync(request.TodoId);
         if (todo is null) return Error.NotFound(description: "Todo not found");
 
