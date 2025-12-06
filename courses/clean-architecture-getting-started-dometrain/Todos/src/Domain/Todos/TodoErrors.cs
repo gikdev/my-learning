c32064673c3d7415b19a628572ffefe132b4f285
gikdev <m.m.bhrm85@gmail.com>
2025-12-06 14:13:54 +0330

    Format using `dotnet format`

--- courses/clean-architecture-getting-started-dometrain/Todos/src/Domain/Todos/TodoErrors.cs
+++ courses/clean-architecture-getting-started-dometrain/Todos/src/Domain/Todos/TodoErrors.cs
@@ -2,9 +2,8 @@ using ErrorOr;
 
 namespace Domain.Todos;
 
-public static class TodoErrors
-{
-    public static readonly Error CannotChangeImportanceIfTodoIsCompleted=Error.Validation(
+public static class TodoErrors {
+    public static readonly Error CannotChangeImportanceIfTodoIsCompleted = Error.Validation(
         "Todo.CannotChangeImportanceIfTaskIsCompleted",
         "The importance of a completed todo can NOT be changed."
     );
