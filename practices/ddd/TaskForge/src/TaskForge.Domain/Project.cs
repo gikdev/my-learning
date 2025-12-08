4dd070b3de737a46444d8cbe98fd3fb715750747
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-12-09 02:43:22 +0330

    Work on domain.

--- practices/ddd/TaskForge/src/TaskForge.Domain/Project.cs
+++ practices/ddd/TaskForge/src/TaskForge.Domain/Project.cs
@@ -1,10 +1,12 @@
+using ErrorOr;
 using TaskForge.Domain.Common;
 
 namespace TaskForge.Domain;
 
 public class Project : Entity {
     private readonly string _title;
-    private readonly ProjectStatus _status;
+    private ProjectStatus _status;
+    public bool IsArchived => _status == ProjectStatus.Archived;
 
     public Project(
         string title,
@@ -13,5 +15,34 @@ public class Project : Entity {
     ) : base(id ?? Guid.NewGuid()) {
         _title = title;
         _status = status ?? ProjectStatus.Active;
+    }
+
+    public void Archive() {
+        _status = ProjectStatus.Archived;
+    }
+
+    public void Activate() {
+        _status = ProjectStatus.Active;
+    }
+
+    public ErrorOr<Task> AddTask(
+        string title,
+        TaskStatus? status = null,
+        Guid? id = null,
+        List<Guid>? labelIds = null,
+        string? description = null,
+        TaskPriority? priority = null
+    ) {
+        if (IsArchived) return ProjectErrors.CantAddTaskWhenProjectIsArchived;
+
+        return new Task(
+            title: title,
+            projectId: Id,
+            status: status,
+            id: id,
+            labelIds: labelIds,
+            description: description,
+            priority: priority
+        );
     }
 }
