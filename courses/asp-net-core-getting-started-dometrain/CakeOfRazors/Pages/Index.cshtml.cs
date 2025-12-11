a79609e3b4c533266eeb0412b26374348ce0be51
gikdev <m.m.bhrm85@gmail.com>
2025-12-11 13:10:42 +0330

    Finish course.

---
+++ courses/asp-net-core-getting-started-dometrain/CakeOfRazors/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CakeOfRazors.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
