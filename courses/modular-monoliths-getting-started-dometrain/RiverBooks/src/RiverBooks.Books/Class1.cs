e6b9af577361cfb86d72a9cb7109accef03e7197
gikdev <m.m.bhrm85@gmail.com>
2025-12-14 15:11:41 +0330

    Make books api work.

---
+++ courses/modular-monoliths-getting-started-dometrain/RiverBooks/src/RiverBooks.Books/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RiverBooks.Books;
+
+public static class BookEndpoints {
+    public static void MapBookEndpoints(this WebApplication app) {
+        app.MapGet("/books", (IBookService bookService) => {
+            return bookService.ListBooks();
+        });
+    }
+}
+
+public record BookDto(Guid Id, string Title, string Author);
+
+internal interface IBookService {
+    IEnumerable<BookDto> ListBooks();
+}
+
+internal class BookService : IBookService {
+    public IEnumerable<BookDto> ListBooks()
+    => [
+        new BookDto(Guid.NewGuid(), "The Fellowship of the Ring", "J.R.R. Tokien"),
+        new BookDto(Guid.NewGuid(), "The Two Towers", "J.R.R. Tokien"),
+        new BookDto(Guid.NewGuid(), "The Return of the King", "J.R.R. Tokien"),
+    ];
+}
+
+public static class BookServiceExtensions {
+    public static IServiceCollection AddBookServices(this IServiceCollection services) {
+        services.AddScoped<IBookService, BookService>();
+
+        return services;
+    }
+}
