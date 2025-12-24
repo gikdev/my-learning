c309fbeea8a84b7068e4aafad94c0e97c0390972
gikdev <m.m.bhrm85@gmail.com>
2025-12-24 12:05:45 +0330

    Finish experiment

--- experiments/json-cs/Builtin/Program.cs
+++ experiments/json-cs/Builtin/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+namespace Builtin;
+
+public class Program
+{
+    public static void Main()
+    {
+        var json = File.ReadAllText("./input.json");
+
+        var settings = new JsonSerializerSettings
+        {
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+        };
+
+        var result = JsonConvert.DeserializeObject<Sth>(json, settings);
+        // var result = JsonSerializer.Deserialize<Sth>(json);
+
+        Console.WriteLine(result?.Print());
+    }
+}
