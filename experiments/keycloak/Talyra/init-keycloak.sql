2627175dc7c7ec07e440dfcd8c048e4c1b8dfafd
gikdev <m.m.bhrm85@gmail.com>
2026-02-03 18:02:30 +0330

    Do the "KeyCloak" experiment.

---
+++ experiments/keycloak/Talyra/init-keycloak.sql
@@ -0,0 +1,5 @@
+-- Create a separate DB for Keycloak
+CREATE DATABASE keycloak;
+-- Optional: create dedicated user for Keycloak
+-- CREATE USER keycloak_user WITH PASSWORD 'StrongPass123';
+-- GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak_user;
---
+++ experiments/keycloak/Talyra/packages.sh
@@ -0,0 +1,7 @@
+dotnet add package Microsoft.AspNetCore.Identity
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+dotnet add package Scalar.AspNetCore
+dotnet add package DotNetEnv
