1b42f1e929ca46bccbf5ab43e064da820c8917a2
gikdev <m.m.bhrm85@gmail.com>
2026-02-05 14:20:20 +0330

    Add OpenTelemetry!

--- courses/pragmatic-rest-apis-jovanovic/DevHabit/DevHabit.Api/Program.cs
+++ courses/pragmatic-rest-apis-jovanovic/DevHabit/DevHabit.Api/Program.cs
@@ -1,7 +1,31 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(res => res
+        .AddService(builder.Environment.ApplicationName)
+    )
+    .WithTracing(tracing => tracing
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+    )
+    .WithMetrics(metrics => metrics
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation()
+    )
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options => {
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
 
 var app = builder.Build();
 
